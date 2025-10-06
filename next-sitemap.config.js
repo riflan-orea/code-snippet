6cdb356ae87631a2550a5423bf73c5663a8c69e7
Riflan Ahmed <info@oreadigital.com>
2025-10-06 18:13:40 +0530

    Update default site URL in next-sitemap.config.js to fallback value

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.SITE_URL || "https://snippet.oreadigital.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
